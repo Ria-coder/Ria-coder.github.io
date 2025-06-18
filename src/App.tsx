1ebb6ce2c3bf35f2a6781f0b625a2810d89835c1
Ria-coder <grushapurtova@gmail.com>
2025-06-18 15:27:21 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
 import Header from './components/Header';
 import Home from './pages/Home';
