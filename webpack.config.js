7df2d4d793a93ba0fc18ba9218b68e169e85cbec
Ria-coder <grushapurtova@gmail.com>
2025-06-18 15:23:19 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,54 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+// Определяем режим (development/production)
+const isDevelopment = process.env.NODE_ENV !== 'production';
+// Имя репозитория для GitHub Pages (замените на своё)
+const repoName = 'LittleTimur.github.io';
 
 module.exports = {
     entry: './src/index.tsx',
     output: {
+        filename: 'index.js',
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/'
+        clean: true,
+        publicPath: '/',
+        assetModuleFilename: 'assets/[name][ext]'
     },
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.jsx']
+        extensions: ['.js', '.ts', '.jsx', '.tsx']
     },
     module: {
         rules: [
             {
                 test: /\.(ts|tsx)$/,
                 exclude: /node_modules/,
-                use: 'ts-loader'
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean),
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            '@babel/preset-typescript'
+                        ]
+                    }
+                }
             },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.hbs|html$/,
+                loader: 'handlebars-loader'
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -31,21 +58,29 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './public/index.html'
+            template: "./src/index.html",
+            filename: "index.html"
         }),
-        new CopyWebpackPlugin({
+        isDevelopment && new ReactRefreshWebpackPlugin(),
+        new ForkTsCheckerWebpackPlugin(),
+        new CopyPlugin({
             patterns: [
-                { from: 'images', to: 'images' }
+                {
+                    from: "./src/img",
+                    to: "img",
+                    noErrorOnMissing: true
+                }
             ]
         })
-    ],
+    ].filter(Boolean),
     devServer: {
         historyApiFallback: true,
         static: {
-            directory: path.join(__dirname, 'public')
+            directory: path.join(__dirname, 'dist'),
+            publicPath: '/'
         },
-        port: 3000,
-        hot: true,
-        open: true
+        port: 5000,
+        open: true,
+        hot: true
     }
 };
